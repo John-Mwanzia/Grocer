a45b5814262a6df45d137279705518e6a1974163
John mwanzia <johnmwanzia277@gmail.com>
2023-07-14 11:07:19 +0300

    create a form validation

---
+++ index.js
@@ -0,0 +1,36 @@
+//create a form validation with js for the following input fields: nameField, emailField,messageField
+
+
+ const nameField = document.getElementById('nameField');
+    const emailField = document.getElementById('emailField');
+    const messageField = document.getElementById('messageField');
+    const submitBtn = document.getElementById('submitBtn');
+    const form = document.getElementById('form');
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        checkInputs();
+    }
+    );
+
+
+    function checkInputs() {
+        if (nameField.value.trim() === '') {
+            alert('Name cannot be blank');
+        }
+        else if (emailField.value.trim() === '') {
+            alert('Email cannot be blank');
+        }
+        else if (messageField.value.trim() === '') {
+            alert('Message cannot be blank');
+        }
+        else {
+            alert('Form submitted successfully');
+        }
+        
+    }
+
+
+
+
