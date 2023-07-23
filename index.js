da9c1f950ee163c609e76f5fd74c29ff09d3570b
John mwanzia <johnmwanzia277@gmail.com>
2023-07-23 14:50:28 +0300

    finish up with navbar

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 hamburger = document.querySelector(".hamburger");
 hamburger.addEventListener("click", () => {
     navLinks = document.querySelector(".nav-links");
+    searchCart = document.querySelector(".search-cart");
     navLinks.classList.toggle("open");
     hamburger.classList.toggle("toggle");
+    searchCart.classList.toggle("open");
 });
